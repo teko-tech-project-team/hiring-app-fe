ff386df5aef78f29c9890ee3690eeaea71ef8cc5
Yasfiq <mohammadyasfiq31@gmail.com>
2023-02-01 15:45:06 +0700

    Add route profile jobseeker and edit profile jobseeker

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //pages
 import LandingPage from "./pages/LandingPage";
-import Home from './pages/Home';
-import ProfileRecruiter from './pages/ProfileRecruiter';
-import EditProfileRecruiter from './pages/EditProfileRecruiter';
-import Chat from './pages/Chat';
+import Home from "./pages/Home";
+import ProfileRecruiter from "./pages/ProfileRecruiter";
+import EditProfileRecruiter from "./pages/EditProfileRecruiter";
+import Chat from "./pages/Chat";
+import ProfileJobSeeker from "./pages/ProfileJobSeeker";
+import EditProfileJobSeeker from "./pages/EditProfileJobSeeker";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>  
-            <Route path='/' element={<LandingPage/>} />
-            <Route path='/home' element={<Home/>} />
-            <Route path='/profile-recruiter' element={<ProfileRecruiter/>} />
-            <Route path='/edit-profile-recruiter' element={<EditProfileRecruiter/>} />
-            <Route path='/chat' element={<Chat/>} />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile-recruiter" element={<ProfileRecruiter />} />
+          <Route path="/profile-job-seeker" element={<ProfileJobSeeker />} />
+          <Route
+            path="/edit-profile-recruiter"
+            element={<EditProfileRecruiter />}
+          />
+          <Route
+            path="/edit-profile-job-seeker"
+            element={<EditProfileJobSeeker />}
+          />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       </Router>
     </>
