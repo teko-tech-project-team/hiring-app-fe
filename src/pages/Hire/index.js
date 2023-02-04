a3f08bc899a4fc1bbb9e732b6c60140892bb9edd
muhammadteguhhernawankurnia <moch.teguhhernawankurnia@gmail.com>
2023-02-04 17:48:24 +0700

    add register recruiter and hire page

--- src/pages/Hire/index.js
+++ src/pages/Hire/index.js
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
+import Footer from '../../components/Footer';
 
 const Hire = () => {
   return (
     <>
       <div className='flex flex-row'>
         <div className='bg-white w-1/3'>
-          <div className='m-12'>
+          <div className='p-12 ml-28'>
             <div className='card card-compact w-96 bg-base-100 shadow-2xl'>
               <figure>
                 <img
@@ -15,7 +16,7 @@ const Hire = () => {
                   alt='img'
                 />
               </figure>
-              <div className='card-body'>
+              <div className='card-body p-12'>
                 <h2 className='card-title'>Louis Tomlinson</h2>
                 <p>Web Developer</p>
                 <p className='text-gray-400'>Freelancer</p>
@@ -34,22 +35,36 @@ const Hire = () => {
 
                 <h2 className='card-title'>Skill</h2>
                 <div className='card-actions justify-start'>
-                  <button className='btn btn-primary'>Phyton</button>
-                  <button className='btn btn-primary'>Laravel</button>
-                  <button className='btn btn-primary'>Golang</button>
-                  <button className='btn btn-primary'>JavaScript</button>
-                  <button className='btn btn-primary'>PHP</button>
-                  <button className='btn btn-primary'>HTML</button>
-                  <button className='btn btn-primary'>C++</button>
-                  <button className='btn btn-primary'>Kotlin</button>
-                  <button className='btn btn-primary'>Swift</button>
+                  <button className='btn btn-secondary border-none'>
+                    Phyton
+                  </button>
+                  <button className='btn btn-secondary border-none'>
+                    Laravel
+                  </button>
+                  <button className='btn btn-secondary border-none'>
+                    Golang
+                  </button>
+                  <button className='btn btn-secondary border-none'>
+                    JavaScript
+                  </button>
+                  <button className='btn btn-secondary border-none'>PHP</button>
+                  <button className='btn btn-secondary border-none'>
+                    HTML
+                  </button>
+                  <button className='btn btn-secondary border-none'>C++</button>
+                  <button className='btn btn-secondary border-none'>
+                    Kotlin
+                  </button>
+                  <button className='btn btn-secondary border-none'>
+                    Swift
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className='flex flex-col w-2/3 container'>
-          <div className='bg-white text-center'>
+          <div className='bg-white text-center p-12'>
             <div class='flex flex-col'>
               <label className='label'>
                 <span className='label-text text-4xl font-bold'>
@@ -62,52 +77,40 @@ const Hire = () => {
                   euismod ipsum et dui rhoncus auctor.
                 </span>
               </label>
-              <div class='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label
-                  class='block text-start tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  for='grid-state'
-                >
-                  Tujuan tentang pesan ini
-                </label>
+              <label className='label'>
+                <span className='label-text mb-2'>Tujuan pesan ini</span>
+              </label>
+              <div class='w-full px-2 mb-6 md:mb-0'>
                 <div class='inline-block relative w-full'>
                   <select
-                    class='block appearance-none w-full bg-white-200 border border-white-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                    class='block appearance-none w-full bg-white-200 border border-white-200 text-gray-500 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
                     id='grid-state'
                   >
                     <option>Projek</option>
                     <option>Kolaborasi</option>
                     <option>Perekrutan</option>
                   </select>
-                  <div class='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500'>
-                    <svg
-                      class='fill-current h-4 w-4'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                    >
-                      <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-                    </svg>
-                  </div>
                 </div>
               </div>
               <label className='label'>
-                <span className='label-text'>Pesan</span>
+                <span className='label-text mt-2'>Pesan</span>
               </label>
-              <input
-                type='text'
-                placeholder='Deskripsikan/jelaskan lebih detail '
-                className='input input-bordered w-50 h-32 text-start m-2'
-              />
+              <textarea
+                className='textarea textarea-bordered m-2 h-[200px]'
+                placeholder='Deskripsikan/jelaskan lebih detail'
+              ></textarea>
               <input
                 type='file'
                 className='file-input file-input-ghost w-full max-w-xs'
               />
             </div>
-            <button className='btn btn-block btn-primary mt-2 p-4'>
+            <button className='btn btn-block btn-secondary border-none mt-2 p-4'>
               Kirim
             </button>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
--- src/pages/RegisterRecruiter/index.js
+++ src/pages/RegisterRecruiter/index.js
@@ -2,7 +2,7 @@ const RegisterRecruiter = () => {
   return (
     <>
       <div className='flex flex-row'>
-        <div className='w-1/2 bg-white'>
+        <div className='w-1/2 bg-white p-12'>
           <img
             src={require('../../assets/images/auth-background.png')}
             className='m-3 h-full'
@@ -10,7 +10,7 @@ const RegisterRecruiter = () => {
           />
         </div>
         <div className='w-1/2 bg-white m-6'>
-          <div class='flex flex-col'>
+          <div class='flex flex-col p-12'>
             <label className='label'>
               <span className='label-text text-4xl font-bold'>
                 Halo, Pewpeople
@@ -78,11 +78,11 @@ const RegisterRecruiter = () => {
               placeholder='Masukan konfirmasi kata sandi'
               className='input input-bordered w-50'
             />
-            <button className='bg-secondary text-white text-xl font-bold w-[48vw] h-[8vh] mt-12 rounded-lg'>
+            <button className='bg-secondary text-white text-xl font-bold w-full h-[8vh] mt-12 rounded-lg '>
               Daftar
             </button>
-            <label className='label justify-center'>
-              <div className='flex flex-row'>
+            <label className='label justify-center '>
+              <div className='flex flex-row '>
                 <p className='text-dark'>Anda sudah punya akun?</p>
                 <p className='text-secondary'>Click disini</p>
               </div>
